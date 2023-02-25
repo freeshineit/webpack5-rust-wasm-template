6a2b27c6155a1d03116fa9f5e4e0e04206414767
ShineShao <xiaoshaoqq@gmail.com>
2023-02-25 17:50:05 +0800

    feat: webpack5

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = (env, argv) => {
+  const isProd = argv.mode === "production";
+  const distPath = path.resolve(__dirname, isProd ? "docs" : "dist");
+  return {
+    devServer: {
+      static: {
+        directory: distPath
+      },
+      compress: isProd,
+      port: 8000
+    },
+    entry: "./app/index.js",
+    output: {
+      path: distPath,
+      filename: "index.js"
+    },
+    module: {
+      rules: []
+    },
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [{ from: "./public", to: distPath }]
+      })
+    ],
+    resolve: {
+      // A little overkill for our tutorial but useful.
+      extensions: [".ts", ".tsx", ".js", ".jsx", ".mts", ".mjs"]
+    }
+  };
+};
