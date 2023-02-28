8f8c88d54110c84b2ba13ab1f14352e59964373d
ShineShao <xiaoshaoqq@gmail.com>
2023-02-28 21:21:38 +0800

    update

--- app/index.ts
+++ app/index.ts
@@ -3,8 +3,20 @@ import "./index.scss";
 // https://developer.mozilla.org/zh-CN/docs/WebAssembly/Loading_and_running
 import("../wasm/pkg").then((module) => {
   console.log("app");
+
+  // +
   const add = module.addition;
   console.log("1 + 5 = ", add(1, 5));
 
-  const sub = console.log("10 - 1 = ", module.subtraction(10, 9));
+  // -
+  const sub = module.subtraction;
+  console.log("10 - 9 = ", sub(10, 9));
+
+  // *
+  const multi = module.multiplication;
+  console.log("5 * 2 = ", multi(5, 2));
+
+  // /
+  const div = module.division;
+  console.log("10 / 2 = ", div(10, 2));
 });
