41a622e8469ce8ab04aeb64283a886d73a177ee9
ShineShao <xiaoshaoqq@gmail.com>
2023-02-28 20:07:00 +0800

    feat: +-*/

--- app/index.ts
+++ app/index.ts
@@ -3,6 +3,8 @@ import "./index.scss";
 // https://developer.mozilla.org/zh-CN/docs/WebAssembly/Loading_and_running
 import("../wasm/pkg").then((module) => {
   console.log("app");
-  const add = module.add;
-  console.log(add(1, 5));
+  const add = module.addition;
+  console.log("1 + 5 = ", add(1, 5));
+
+  const sub = console.log("10 - 1 = ", module.subtraction(10, 9));
 });
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -3,8 +3,23 @@ use wasm_bindgen::prelude::*;
 
 // #[wasm_bindgen] 属性 是把当前函数暴露给js使用
 #[wasm_bindgen]
-pub fn add(left: usize, right: usize) -> usize {
+pub fn addition(left: usize, right: usize) -> usize {
     left + right
+}
+
+#[wasm_bindgen]
+pub fn subtraction(left: usize, right: usize) -> usize {
+    left - right
+}
+
+#[wasm_bindgen]
+pub fn division(left: usize, right: usize) -> usize {
+    left / right
+}
+
+#[wasm_bindgen]
+pub fn multiplication(left: usize, right: usize) -> usize {
+    left * right
 }
 
 #[cfg(test)]
@@ -12,8 +27,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
+    fn addition_works() {
+        let result = addition(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn subtraction_works() {
+        let result = subtraction(10, 8);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn division_works() {
+        let result = division(10, 2);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn multiplication_works() {
+        let result = multiplication(2, 5);
+        assert_eq!(result, 10);
     }
 }
