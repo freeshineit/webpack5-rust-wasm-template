9df508aebb5e602ae6ff136f05285aa3bc9a9ebf
ShineShao <xiaoshaoqq@gmail.com>
2023-03-01 17:23:13 +0800

    update demo

--- app/index.ts
+++ app/index.ts
@@ -6,21 +6,21 @@ import("../wasm/pkg").then((module) => {
 
   // +
   // function addition(left: number, right: number): number
-  const add = module.addition;
+  const add = module.ArithmeticOperation.addition;
   console.log("1 + 5 = ", add(1, 5));
 
   // -
   // function subtraction(left: number, right: number): number
-  const sub = module.subtraction;
+  const sub = module.ArithmeticOperation.subtraction;
   console.log("10 - 9 = ", sub(10, 9));
 
   // *
   // function multiplication(left: number, right: number): number
-  const multi = module.multiplication;
+  const multi = module.ArithmeticOperation.multiplication;
   console.log("5 * 2 = ", multi(5, 2));
 
   // /
   // function division(left: number, right: number): number
-  const div = module.division;
+  const div = module.ArithmeticOperation.division;
   console.log("10 / 2 = ", div(10, 2));
 });
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -9,56 +9,66 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // #[wasm_bindgen] 属性 是把当前函数暴露给js使用
 
-/// Addition.
-///
-/// # Examples
-///
-/// ```
-/// let x = addition(1, 2);
-///
-/// ```
 #[wasm_bindgen]
-pub fn addition(left: usize, right: usize) -> usize {
-    left + right
-}
+struct ArithmeticOperation {}
 
-/// Subtraction.
 ///
-/// # Examples
+/// Arithmetic Operation (+,-,*,/)
 ///
-/// ```
-/// let x = subtraction(10, 9);
 ///
-/// ```
 #[wasm_bindgen]
-pub fn subtraction(left: usize, right: usize) -> usize {
-    left - right
-}
+impl ArithmeticOperation {
+    /// Addition.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let x = ArithmeticOperation::addition(1, 2);
+    ///
+    /// ```
+    #[wasm_bindgen]
+    pub fn addition(left: usize, right: usize) -> usize {
+        left + right
+    }
 
-/// Division.
-///
-/// # Examples
-///
-/// ```
-/// let x = division(10, 5);
-///
-/// ```
-#[wasm_bindgen]
-pub fn division(left: usize, right: usize) -> usize {
-    left / right
-}
+    /// Subtraction.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let x = ArithmeticOperation::subtraction(10, 9);
+    ///
+    /// ```
+    #[wasm_bindgen]
+    pub fn subtraction(left: usize, right: usize) -> usize {
+        left - right
+    }
 
-/// Multiplication.
-///
-/// # Examples
-///
-/// ```
-/// let x = multiplication(2, 5);
-///
-/// ```
-#[wasm_bindgen]
-pub fn multiplication(left: usize, right: usize) -> usize {
-    left * right
+    /// Division.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let x = ArithmeticOperation::division(10, 5);
+    ///
+    /// ```
+    #[wasm_bindgen]
+    pub fn division(left: usize, right: usize) -> usize {
+        left / right
+    }
+
+    /// Multiplication.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let x = ArithmeticOperation::multiplication(2, 5);
+    ///
+    /// ```
+    #[wasm_bindgen]
+    pub fn multiplication(left: usize, right: usize) -> usize {
+        left * right
+    }
 }
 
 #[cfg(test)]
@@ -67,25 +77,25 @@ mod tests {
 
     #[test]
     fn addition_works() {
-        let result = addition(2, 2);
+        let result = ArithmeticOperation::addition(2, 2);
         assert_eq!(result, 4);
     }
 
     #[test]
     fn subtraction_works() {
-        let result = subtraction(10, 8);
+        let result = ArithmeticOperation::subtraction(10, 8);
         assert_eq!(result, 2);
     }
 
     #[test]
     fn division_works() {
-        let result = division(10, 2);
+        let result = ArithmeticOperation::division(10, 2);
         assert_eq!(result, 5);
     }
 
     #[test]
     fn multiplication_works() {
-        let result = multiplication(2, 5);
+        let result = ArithmeticOperation::multiplication(2, 5);
         assert_eq!(result, 10);
     }
 }
