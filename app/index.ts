2c79a1318df2f01b4ce8c3c6e1ded44579d5d62f
ShineShao <xiaoshaoqq@gmail.com>
2023-04-09 18:59:16 +0800

    ci: add eslint prettier

--- app/index.ts
+++ app/index.ts
@@ -1,26 +1,26 @@
-import "./index.scss";
+import './index.scss';
 
 // https://developer.mozilla.org/zh-CN/docs/WebAssembly/Loading_and_running
-import("../wasm/pkg").then((module) => {
-  console.log("app");
+import('../wasm/pkg').then((module) => {
+  console.log('app');
 
   // +
   // function addition(left: number, right: number): number
   const add = module.ArithmeticOperation.addition;
-  console.log("1 + 5 = ", add(1, 5));
+  console.log('1 + 5 = ', add(1, 5));
 
   // -
   // function subtraction(left: number, right: number): number
   const sub = module.ArithmeticOperation.subtraction;
-  console.log("10 - 9 = ", sub(10, 9));
+  console.log('10 - 9 = ', sub(10, 9));
 
   // *
   // function multiplication(left: number, right: number): number
   const multi = module.ArithmeticOperation.multiplication;
-  console.log("5 * 2 = ", multi(5, 2));
+  console.log('5 * 2 = ', multi(5, 2));
 
   // /
   // function division(left: number, right: number): number
   const div = module.ArithmeticOperation.division;
-  console.log("10 / 2 = ", div(10, 2));
+  console.log('10 / 2 = ', div(10, 2));
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,60 @@
-const path = require("path");
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
-  const isProd = argv.mode === "production";
-  const distPath = path.resolve(__dirname, isProd ? "docs" : "dist");
+  const isProd = argv.mode === 'production';
+  const distPath = path.resolve(__dirname, isProd ? 'docs' : 'dist');
   return {
     devServer: {
       static: {
-        directory: distPath
+        directory: distPath,
       },
       compress: isProd,
-      port: 8000
+      port: 8000,
     },
-    entry: "./app/index.ts",
+    entry: './app/index.ts',
     output: {
       path: distPath,
-      filename: "index.js",
-      webassemblyModuleFilename: "index.wasm"
+      filename: 'index.js',
+      webassemblyModuleFilename: 'index.wasm',
     },
     module: {
       rules: [
         {
           test: /\.s[ac]ss$/i,
-          use: ["style-loader", "css-loader", "sass-loader"]
+          use: ['style-loader', 'css-loader', 'sass-loader'],
         },
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"]
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.(ts|js)?$/,
-          use: "swc-loader",
-          exclude: /node_modules/
-        }
-      ]
+          use: 'swc-loader',
+          exclude: /node_modules/,
+        },
+      ],
     },
     plugins: [
       new CopyWebpackPlugin({
-        patterns: [{ from: "./public", to: distPath }]
+        patterns: [{ from: './public', to: distPath }],
       }),
       // We point our WasmPackPlugin to the location of the
       // the crates `Cargo.toml` file. Never the root file.
       new WasmPackPlugin({
-        crateDirectory: path.join(__dirname, "wasm"),
-        outDir: path.join(__dirname, "wasm/pkg")
+        crateDirectory: path.join(__dirname, 'wasm'),
+        outDir: path.join(__dirname, 'wasm/pkg'),
         // extraArgs: ""
-      })
+      }),
     ],
     resolve: {
       // A little overkill for our tutorial but useful.
-      extensions: [".ts", ".tsx", ".js", ".jsx", ".mts", ".mjs"]
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.mts', '.mjs'],
     },
     experiments: {
       asyncWebAssembly: true,
-      syncWebAssembly: true
-    }
+      syncWebAssembly: true,
+    },
   };
 };
